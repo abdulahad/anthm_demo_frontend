28d5771331d90142b74edff3eec0435aa0f8d540
Ahad Jeyemby <abdulahad.jmb@gmail.com>
2021-11-29 11:31:28 -0500

    fixes

--- src/Navigation.js
+++ src/Navigation.js
@@ -6,12 +6,12 @@ function Navigation() {
       <Col>
         <Navbar bg="light" variant="light" expand="lg">
           <Container>
-            <Navbar.Brand href="/">Anthm Demo Project</Navbar.Brand>
+            <Navbar.Brand href="/">Employee Management Tool</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
                 <Nav.Link href="/">Employees List</Nav.Link>
-                <Nav.Link href="/create">Create Employees</Nav.Link>
+                <Nav.Link href="/create">New Employees</Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Container>
